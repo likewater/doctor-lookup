33ae9d1035d110228b28f75205fa3661d8ceda1f
likewater <lakebayomackway@gmail.com>
2017-04-23 16:30:46 -0700

    add images

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -3,7 +3,7 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 var doctorDisplay = function(doctors) {
   doctors.forEach(function(doctor){
       $('.showDoctors').append(
-        "<table class='table table-striped'><thead><th>First Name</th><th>Last Name</th><th>Specialty</th></thead><tbody><td><p>Dr. " + doctor.profile.first_name + "</p></td><td><p> " + doctor.profile.last_name + "</p></td><td><p> " + doctor.profile.specialty_uid + "</p></td></tbody></table>"
+        "<table class='table table-striped'><thead><th>First Name</th><th>Last Name</th><th>Title</th><th>Picture</th></thead><tbody><td><p>Dr. " + doctor.profile.first_name + "</p></td><td><p> " + doctor.profile.last_name + "</p></td><td><p> " + doctor.profile.title + "</p></td><td><img src=" + doctor.profile.image_url + "></img></td></tbody></table>"
 
 
 
