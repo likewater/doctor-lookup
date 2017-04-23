981a14bd00b4bd3911bda42f32c8db0af3456fbf
likewater <lakebayomackway@gmail.com>
2017-04-23 15:20:08 -0700

    add first working js, includes commented out errors and code for posterity

--- js/doctor.js
+++ js/doctor.js
@@ -1,35 +1,23 @@
 var apiKey = require('./../.env').apiKey;
+var resourceUrl = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey;
 
-Doctor = function(){
+function Doctor(){//is this correct?
 }
-Doctor.prototype.getDoctor = function(location, specialty, insurance) {
 
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?location=' + location + '&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=647520a0b46d95fb8ef2cc58492b69a7').then(function(response) {
+Doctor.prototype.getDoctor = function(location, doctorDisplay){
 
+$.get(resourceUrl).then(function(response) {
+  doctorDisplay(response.data);
+  //console.log(response.data);
+  //var result = JSON.stringify(response);
+  //var doctors = response;
+  //response.doctors.forEach(function(doctor) {
+  // $('.showDoctors').append("<p>" + response.data + " test" + response.data.first_name + "</p>");
+  // console.log(response.data);
 
-
-  // $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=' + doctor + '&distance=25&stolenness=proximity').then(function(response) {
-
-    $('.showDoctors').text(response(function(location, specialty, insurance) {
-      $('.showDoctors').append("<p>Name" + data.practices.name  + "</p>")
-
-      // "><span><strong>Description</strong>:</span>" + " " + bike.title + "<br>" + "<span><strong>Serial number:</strong></span>" + " " +  bike.serial + "<br>" + "<span><strong>Date reported stolen:</strong></span>" + " " + new Date(bike.date_stolen * 1000) + "</p><br>");
-      // console.log("response test: " + response);
-    }));
-
+//});
   }).fail(function(error) {
       $('.showDoctors').text(error.responseJSON.message);
-  });
+   });
 };
-
-// Bike.prototype.getLocationMake = function(location, manufacturer) {
-//   $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=' + manufacturer + '&location=' + location + '&distance=25&stolenness=proximity').then(function(response) {
-//     $('.showBikes').text(response.bikes.forEach(function(bike) {
-//       $('.showBikes').append("<a href=https://bikeindex.org/bikes/" + bike.id  + "><span><strong>Description</strong>:</span>" + " " + bike.title + "<br>" + "<span><strong>Serial number:</strong></span>" + " " +  bike.serial + "<br>" + "<span><strong>Date reported stolen:</strong></span>" + " " + new Date(bike.date_stolen * 1000) + "</p><br></a>");
-//     }));
-//   })
-//     .fail(function(error) {
-//       $('.showDoctors').text(error.responseJSON.message);
-//   });
-// };
 exports.doctorModule = Doctor;
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,12 +1,29 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
+var doctorDisplay = function(doctors) {
+  doctors.forEach(function(doctor){
+      $('.showDoctors').append(
+
+        "<p>Dr. " + doctor.profile.first_name + "</p>"
+
+      );
+    });
+}
 
 $(document).ready(function() {
   var currentDoctorObject = new Doctor();
 
-  $('#doctorSearch').click(function() {
-    var doctor = $('#doctor').val();
-    $('#doctor').val();
-    currentDoctorObject.getDoctor(doctor);
+  $('#formFlow').submit(function() {
+    event.preventDefault();
+    var location = $('#location').val();
+    $('#location').val("");
+    // var user_location = $('#user_location').val();
+    // $('#user_location').val();
+    // var specialty_uid = $('#specialty_uid').val();
+    // $('#specialty_uid').val();
+    // var insurance_uid = $('#insurance_uid').val();
+    // $('#insurance_uid').val();
+    // currentDoctorObject.getDoctor(specialty_uid, insurance_uid, location, user_location);
+    currentDoctorObject.getDoctor(location, doctorDisplay);
   });
 });
